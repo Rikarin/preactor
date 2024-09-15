4584828e2e66650a6c51380656b8db62cec51208
Jiu <rene@cincura.eu>
2024-09-15 17:47:30 +0200

    Init docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://rikarin.app/',
+	base: 'preactor',
+	integrations: [
+		starlight({
+			title: 'Preactor',
+			social: {
+				github: 'https://github.com/rikarin/preactor',
+			},
+			sidebar: [
+				{
+					label: 'Getting Started',
+					autogenerate: { directory: 'getting-started' },
+					// items: [
+					// 	// Each item here is one entry in the navigation menu.
+					// 	{ label: 'Example Guide', slug: 'guides/example' },
+					// ],
+				},
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
---
+++ docs/src/content/docs/getting-started/installation.md
@@ -0,0 +1,42 @@
+---
+title: Installation
+description: A guide in my new Starlight docs site.
+---
+
+
+clone repo 
+```sh
+cd Assets/Plugins
+git clone https://github.com/rikarin/preactor Preactor
+```
+
+
+create App directory with base stuff
+```sh
+mkdir App
+cd App
+npm install @rikarin/preactor
+```
+
+run the tsc, tailwind, esbuild
+```sh
+npm run start
+```
+
+
+
+Create new global object in scene
+`Preactor/Preactor Global`
+
+
+then create renderer object and import `index.js`
+`Preactor/Preactor Renderer`
+
+
+
+
+
+
+## Further reading
+
+- Read [about how-to guides](https://diataxis.fr/how-to-guides/) in the Diátaxis framework
