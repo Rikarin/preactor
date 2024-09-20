a363052469c722b9a2a2c46ca3b053540c584f57
Jiu <rene@cincura.eu>
2024-09-20 08:47:24 +0200

    fix: restart esbuild when new file is created

--- Preactor/solution/esbuild.mjs
+++ Preactor/solution/esbuild.mjs
@@ -1,25 +1,54 @@
 import glob from 'tiny-glob';
 import * as esbuild from 'esbuild';
+import chokidar from 'chokidar';
 import { importTransformPlugin, watchOutputPlugin } from '@rikarin/preactor/scripts/esbuild/index.mjs';
 
-let entryPoints = await glob('./dist/tsc/Views/**/index.js');
-let mainCtx = await esbuild.context({
-  entryPoints: entryPoints,
-  bundle: true,
-  plugins: [importTransformPlugin, watchOutputPlugin],
-  inject: ['@rikarin/preactor/dist/index.js'],
-  alias: {
-    preactor: '@rikarin/preactor',
-    preact: '@rikarin/preactor'
-  },
-  outdir: '../Assets/Views'
-});
-await mainCtx.watch();
+const SOURCE_PATH = './dist/tsc/Views/**/index.js';
+
+async function prepare() {
+  try {
+    let entryPoints = await glob(SOURCE_PATH);
+    return await esbuild.context({
+      entryPoints: entryPoints,
+      bundle: true,
+      plugins: [importTransformPlugin, watchOutputPlugin],
+      inject: ['@rikarin/preactor/dist/index.js', './dist/tsc/globals.js'],
+      alias: {
+        preactor: '@rikarin/preactor',
+        preact: '@rikarin/preactor/preact'
+      },
+      outdir: '../Assets/Views'
+    });
+  } catch { }
+}
+
+let main = await prepare();
+await main?.watch();
 console.log('Watching for changes...');
 
-// Editor not supported, yet
+
+// Watch for new files added
+const watcher = chokidar.watch('./dist', {
+  persistent: true,
+  ignoreInitial: true
+});
+
+let debounceTimeout;
+const DEBOUNCE_INTERVAL = 100; // ms
+watcher.on('add', async () => {
+  clearTimeout(debounceTimeout);
+
+  debounceTimeout = setTimeout(async () => {
+    console.log('Detected new files, rebuilding...');
+    await main?.dispose();
+    main = await prepare();
+    await main?.watch();
+  }, DEBOUNCE_INTERVAL);
+});
+
+// Editor isn't supported, yet
 // try {
-// 	entryPoints = await glob("./dist/tsc/Editor/**/*.js");
+// 	entryPoints = await glob("./dist/tsc/Editor/**/index.js");
 // 	let editorCtx = await esbuild.context({
 // 		entryPoints: entryPoints,
 // 		bundle: true,
