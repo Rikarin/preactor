9eee88e292f27cecbe7e7af50b19e7e6d4737867
Jiu <rene@cincura.eu>
2024-12-29 21:18:00 +0100

    fix: package lock

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,8 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://rikarin.app/',
-	base: 'preactor',
+	site: 'https://preactor.rikarin.app/',
+	base: '/',
 	integrations: [
 		starlight({
 			title: 'Preactor',
